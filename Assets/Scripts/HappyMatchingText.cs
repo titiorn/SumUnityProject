bbf265c331dcda7eff736bd7c39ec31a5821d4bf
titiorn <thitiorn.uss@gmail.com>
2024-11-24 13:12:55 +0000

    Create 2nd Script

--- Assets/Scripts/HappyMatchingText.cs
+++ Assets/Scripts/HappyMatchingText.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HappyMatchCorrectName : MonoBehaviour
+public class HappyMatchingText : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
