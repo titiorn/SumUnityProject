83a4fa766d5bbf372b669fe7843aa59a8e898978
titiorn <thitiorn.uss@gmail.com>
2024-11-29 12:35:22 +0000

    Develop 2nd script
    For drag and Drop + Add animation. mp4 (test)

--- Assets/Scripts/HappyMatchingText.cs
+++ Assets/Scripts/HappyMatchingText.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Video;
 
 public class HappyMatchingText : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
@@ -12,14 +13,17 @@ public class HappyMatchingText : MonoBehaviour, IBeginDragHandler, IDragHandler,
         public TextMeshProUGUI textObj; //Text to be dragged
         [HideInInspector] public RectTransform rectTransform;
         [HideInInspector] public CanvasGroup canvasGroup;
+
+        public GameObject placer; //Placer for each text
+        [HideInInspector] public bool isPlacedCorrectly; //Status of Placement
     }
 
     public List<DraggableText> draggableTexts; //List of draggable texts
+    //public Animator sceneAnimator; //Animator for the scene Animation
+    public VideoPlayer videoPlayer;
     private Canvas canvas;
     private DraggableText currentDraggableText;
 
-    //private RectTransform rectTransform;
-    //private CanvasGroup canvasGroup;
     
     private void Awake()
     {
@@ -30,11 +34,9 @@ public class HappyMatchingText : MonoBehaviour, IBeginDragHandler, IDragHandler,
         {
             draggable.rectTransform = draggable.textObj.GetComponent<RectTransform>();
             draggable.canvasGroup = draggable.textObj.gameObject.AddComponent<CanvasGroup>();
+            draggable.isPlacedCorrectly = false; //Initializa placement status
         }
 
-        /*rectTransform = GetComponent<RectTransform>();*/
-        /*canvasGroup = GetComponent<CanvasGroup>();*/
-        /*canvas = GetComponentInParent<Canvas>();*/
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -42,12 +44,10 @@ public class HappyMatchingText : MonoBehaviour, IBeginDragHandler, IDragHandler,
         currentDraggableText = GetDraggableTextUnderPointer(eventData);
         if(currentDraggableText != null)
         {
-            currentDraggableText.canvasGroup.alpha = 0.6f;
+            currentDraggableText.canvasGroup.alpha = 0.6f; //Control the visibility of text while dragging
             currentDraggableText.canvasGroup.blocksRaycasts = false; //Allow text to be dragged through other UI elements
         }
 
-        //canvasGroup.alpha = 0.6f;
-        //canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -58,8 +58,6 @@ public class HappyMatchingText : MonoBehaviour, IBeginDragHandler, IDragHandler,
             currentDraggableText.rectTransform.localPosition = localPoint;
         }
 
-        //RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, eventData.position, canvas.worldCamera, out Vector2 localPoint);
-        //rectTransform.localPosition = localPoint;
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -68,10 +66,32 @@ public class HappyMatchingText : MonoBehaviour, IBeginDragHandler, IDragHandler,
         {
             currentDraggableText.canvasGroup.alpha = 1f;
             currentDraggableText.canvasGroup.blocksRaycasts = true;
+
+            //Check if the text is placed in the correct spot
+            if(RectTransformUtility.RectangleContainsScreenPoint(currentDraggableText.placer.GetComponent<RectTransform>(), eventData.position, canvas.worldCamera))
+            {
+                currentDraggableText.rectTransform.localPosition = currentDraggableText.placer.GetComponent<RectTransform>().localPosition;
+                currentDraggableText.isPlacedCorrectly = true;
+            }
+            else
+            {
+                currentDraggableText.isPlacedCorrectly = false;
+            }
+
             currentDraggableText = null;
+
+            //Check if all texts are correctly placed
+            /*if(AllTextsPlacedCorrectly())
+            {
+                PlayAnimation();
+            }*/
+
+            if(AllTextsPlacedCorrectly())
+            {
+                PlayVideo();
+            }
+
         }
-        //canvasGroup.alpha = 1f;
-        //canvasGroup.blocksRaycasts = true;
     }
     
     private DraggableText GetDraggableTextUnderPointer(PointerEventData eventData)
@@ -84,5 +104,42 @@ public class HappyMatchingText : MonoBehaviour, IBeginDragHandler, IDragHandler,
             }
         }
         return null;
+    }
+
+    private bool AllTextsPlacedCorrectly()
+    {
+        foreach(var draggable in draggableTexts)
+        {
+            if(!draggable.isPlacedCorrectly)
+            {
+                return false; //If any text is not correctly placed, return false
+            }
+        }
+        return true; //All texts are correctly placed
+    }
+
+    /*private void PlayAnimation()
+    {
+        //Trigger the scene animation
+        if(sceneAnimator != null)
+        {
+            sceneAnimator.SetTrigger("PlayAnimation");
+        }
+        else
+        {
+            Debug.LogError("Animator component is not assigned!");
+        }
+    }*/
+
+    private void PlayVideo()
+    {
+        if(videoPlayer != null)
+        {
+            videoPlayer.Play();
+        }
+        else
+        {
+            Debug.LogError("VideoPlayer component is not assigned!");
+        }
     }
 }
