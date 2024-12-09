e89304a50064cebf864c2e15cd3b272d668bd09f
ChakraOdyssey <elizabethannedunlap@gmail.com>
2024-12-09 17:03:02 +0000

    Added Set Text Active to Render Texture Script
    with RAMA's help! And it was Orn's idea.

--- Assets/Scripts/ManageRenderTextureOnClick.cs
+++ Assets/Scripts/ManageRenderTextureOnClick.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class ManageRenderTextureOnClick : MonoBehaviour
 {
     [SerializeField]
     private RawImage myImagePanel;
+
+    public string info_text;
+    public GameObject textObject;
+    public TextMeshProUGUI infoText;
+
+    private bool isPanelActive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +29,12 @@ public class ManageRenderTextureOnClick : MonoBehaviour
     void OnMouseDown()
     {
         myImagePanel.texture = transform.GetChild(0).GetComponent<Camera>().targetTexture;
+        isPanelActive = !isPanelActive;
+
+        textObject.SetActive(isPanelActive);
+        infoText.text = info_text;
+
+        //gameObject.GameObject = transform.GetChild(0).GetComponent<GameObject>().targetGameObject;
     }
 
 }
